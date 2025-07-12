772707beafa97da7b90844cb5cfb91aeaedb9508
Jan1986-cloud <jan@pvmonteur.nl>
2025-07-12 09:31:25 +0200

    Fix: Final attempt to fix availability logic and session persistence

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ app.use(
     saveUninitialized: true,
     cookie: { 
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax' 
+      sameSite: 'lax',
+      maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     },
   })
 );
