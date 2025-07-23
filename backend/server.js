c233e6f9d5b2dc11b9b25e9623694ce138c7abfc
Jan1986-cloud <jan@pvmonteur.nl>
2025-07-23 09:07:10 +0200

    fix(auth): set definitive 'lax' cookie policy for reverse proxy

--- backend/server.js
+++ backend/server.js
@@ -85,7 +85,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
       secure: isProduction,
-      sameSite: isProduction ? 'none' : 'lax',
+      sameSite: 'lax',
       maxAge: 30 * 24 * 60 * 60 * 1000 // 30 dagen
     },
 }));
